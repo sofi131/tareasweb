7477d8c1fd71c4321cbbc3e1479556b82dec30d6
sofi131 <sofiasanmartin131@gmail.com>
2024-02-28 19:49:11 +0100

    Si es admin o usuario

--- src/main/java/com/ceica/tareasweb/servlets/LoginServlet.java
+++ src/main/java/com/ceica/tareasweb/servlets/LoginServlet.java
@@ -12,14 +12,15 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "loginServlet", value = "/login")
 public class LoginServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+//doGet y doPost
+     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         request.getRequestDispatcher("login.jsp").forward(request,response);
     }
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String user=request.getParameter("user");
         String pass=request.getParameter("password");
         TaskController taskController=new TaskController();
-
         if(taskController.login(user,pass)){
             //request.getRequestDispatcher("admin.jsp").forward(request,response);
             response.sendRedirect("admin");
@@ -30,3 +31,4 @@ public class LoginServlet extends HttpServlet {
         //request.getRequestDispatcher("login.jsp").forward(request,response);
     }
 }
+
